7e99d88c380603efba2fff7e1d485a3f506b9437
Pieter K de Villiers <pieter@hey.com>
2023-08-28 10:54:53 +0100

    Start CRUD

---
+++ src/components/AOLForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const AOLForm = ({ onAddAOL }) => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newAOL = { name, description };
+
+    try {
+      const response = await fetch('/snapshots/api/v1/aols', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newAOL),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        onAddAOL(data); // Update the state with the new AOL data
+        setName('');
+        setDescription('');
+      } else {
+        console.error('Failed to create AOL');
+      }
+    } catch (error) {
+      console.error('Error creating AOL:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Description:
+        <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Create AOL</button>
+    </form>
+  );
+};
+
+export default AOLForm;
--- src/components/SnapshotItem.js
+++ src/components/SnapshotItem.js
@@ -3,9 +3,10 @@ import React from 'react'
 const SnapshotItem = ({snapshot}) => {
   return (
     <div>
-        <h3>{snapshot.body}</h3>
-        <h4>{snapshot.area_of_life}</h4>
+      <div>
         <h4>{snapshot.created}</h4>
+      </div>
+        <p>{snapshot.body}</p>
     </div>
   )
 }
